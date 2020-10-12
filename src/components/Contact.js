15dc148a8e8327e569560b17d5fbf6702a86a1dd
Alexandre POIRRIER <alex.poirrier@gmail.com>
2020-10-12 12:29:35 +0200

    ADD states to the application

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { Component }from 'react';
 import PropTypes from 'prop-types';
 import './Contact.css';
 
 
 
-function Contact(props){
-    return (
-        <figure className="Contact">
-            <img src={props.avatar} alt="avatar" className="avatar"/>
-            <div>
-    <figcaption className="name">{props.name}</figcaption>
-            <div className="status">
-                <div className={props.online ? "status-online" : "status-offline"}/>
-                <span className="status-text">{props.online ? "Online" : "Offline"}</span>
+class Contact extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            online :props.online
+        }
+    }
+    render() {
+        return (
+            <figure className="Contact">
+                <img src={this.props.avatar} alt="avatar" className="avatar" />
+                <div>
+                    <figcaption className="name">{this.props.name}</figcaption>
+                    <div 
+                    className="status"
+                    onClick = {event =>{
+                        const status = !this.state.online;
+                        this.setState({online: status});
+                    }}
+                    >
+                        <div className={this.state.online ? "status-online" : "status-offline"} />
+                        <span className="status-text">{this.state.online ? "Online" : "Offline"}</span>
+                    </div>
                 </div>
-            </div>
-        </figure>
-    )
+            </figure>
+        )
+    }
 }
-
 Contact.propTypes = {
     name: PropTypes.string.isRequired,
     avatar: PropTypes.string.isRequired,
